c7a00ee99c22c1db1616df2cbf91e817cbf85f1e
francesco-unitn <francesco.verdolin@studenti.unitn.it>
2025-12-02 18:04:38 +0100

    Removed Rusteroids/target

--- Rusteroids/target/debug/deps/Rusteroids-5a0f5176043f75fc.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroids\target\debug\deps\Rusteroids-5a0f5176043f75fc.d: src\main.rs
-
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroids\target\debug\deps\libRusteroids-5a0f5176043f75fc.rmeta: src\main.rs
-
-src\main.rs:
--- Rusteroids/target/debug/deps/Rusteroids-94b3a88d3527b725.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroids\target\debug\deps\Rusteroids-94b3a88d3527b725.d: src\main.rs
-
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroids\target\debug\deps\libRusteroids-94b3a88d3527b725.rmeta: src\main.rs
-
-src\main.rs:
