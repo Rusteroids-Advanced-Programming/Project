d25105e1722ebbf6ec6740dd9d3d0a554d93bbc7
Federico Valsecchi <116191955+propoto@users.noreply.github.com>
2026-06-18 23:30:32 +0200

    README updated

--- Rusteroids/target/debug/deps/Rusteroids-c6246eea4c80b02a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroids\target\debug\deps\Rusteroids-c6246eea4c80b02a.d: src\main.rs
-
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroids\target\debug\deps\libRusteroids-c6246eea4c80b02a.rmeta: src\main.rs
-
-src\main.rs:
--- Rusteroids/target/debug/deps/Rusteroids-d15d56f5ca3f723d.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroids\target\debug\deps\Rusteroids-d15d56f5ca3f723d.d: src\main.rs
-
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroids\target\debug\deps\libRusteroids-d15d56f5ca3f723d.rmeta: src\main.rs
-
-src\main.rs:
